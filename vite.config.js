f1b0ab0792b2cd81b8bb9074cb047a51f95d29ba
fragum96-svg <fra.gum.96@gmail.com>
2026-07-15 16:42:36 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,22 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("xlsx")) {
+              return "vendor-xlsx";
+            }
+            if (id.includes("papaparse")) {
+              return "vendor-papaparse";
+            }
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
 });
